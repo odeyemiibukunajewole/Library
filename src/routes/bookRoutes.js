16d7bf41ce4cfc59a0b1d3bb35df793a595d5825
Odeyemi Ibukun <odeyemiibukuna@gmail.com>
2020-04-14 01:04:56 +0100

    initial commit

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bookRouter = express.Router();
+function router(nav) {
+  const books = [{
+    author: 'Chinua Achebe',
+    title: 'Things Fall Apart',
+    read: false
+  },
+  {
+    author: 'Hans Christian Andersen',
+    title: 'Fairy tales',
+    read: false
+  },
+  {
+    author: 'Dante Alighieri',
+    title: 'The Divine Comedy',
+    read: false,
+  },
+  {
+    author: 'Jane Austen',
+    title: 'Pride and Prejudice',
+    read: false,
+  },
+  {
+    author: 'Samuel Beckett',
+    title: 'Molloy, Malone Dies, The Unnamable, the trilogy',
+    read: false
+  },
+  {
+    author: 'Ralph Ellison',
+    title: 'Invisible Man',
+    read: false
+  },
+  {
+    author: 'Euripides',
+    title: 'Medea',
+    read: false
+  },
+  {
+    author: 'William Faulkner',
+    title: 'The Sound and the Fury',
+    read: false
+  },
+  {
+    author: 'Gustave Flaubert',
+    title: 'Madame Bovary',
+    read: false
+  }
+  ];
+
+  bookRouter.route('/').get((req, res) => {
+    res.render('booksListView',
+      {
+        title: 'Library',
+        nav,
+        books
+      });
+  });
+
+  bookRouter.route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      res.render('bookView',
+        {
+          title: 'Library',
+          nav,
+          books: books[id]
+        });
+    });
+  return bookRouter;
+}
+
+
+module.exports = router;
